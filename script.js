c6193d7757a9fb890815488a4d655850f12eb939
ekaterine-nanobashvili <ekaterine.nanobashvili@gmail.com>
2022-11-30 23:16:44 +0400

    Baseline visuals complete

---
+++ script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+    
+  });
