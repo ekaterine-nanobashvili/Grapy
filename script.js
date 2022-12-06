f59a253d8e73e810f8f7a3aad5e20cb87ea1914d
ekaterine-nanobashvili <ekaterine.nanobashvili@gmail.com>
2022-12-07 02:19:38 +0400

    added animate on scroll functionality

--- script.js
+++ script.js
@@ -285,3 +285,77 @@ ourMission1.addEventListener('click', function() {
 ourMission2.addEventListener('click', function() {
     missionSection.scrollIntoView(scrollIntoViewOptions) 
 });
+
+// JS on scroll animations
+// const scrollOffset = 100;
+ 
+// const scrollElement = document.querySelector(".js-scroll");
+ 
+// const elementInView = (el, offset = 0) => {
+//   const elementTop = el.getBoundingClientRect().top;
+ 
+//   return (
+//     elementTop <= 
+//     ((window.innerHeight || document.documentElement.clientHeight) - offset)
+//   );
+// };
+ 
+// const displayScrollElement = () => {
+//   scrollElement.classList.add('scrolled');
+// }
+ 
+// const hideScrollElement = () => {
+//   scrollElement.classList.remove('scrolled');
+// }
+ 
+// const handleScrollAnimation = () => {
+//   if (elementInView(scrollElement, scrollOffset)) {
+//       displayScrollElement();
+//   } else {
+//     hideScrollElement();
+//   }
+// }
+ 
+// window.addEventListener('scroll', () => {
+//   handleScrollAnimation();
+// })
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
